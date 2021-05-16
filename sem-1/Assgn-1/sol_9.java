47b2276c8b90e0cd19520bc596ab3c4c921c5f9a
ARMAN ALI KHAN <72055054+Arman-ali-khan-786@users.noreply.github.com>
2021-05-16 12:47:52 +0530

    created sol_9.java file

---
+++ sem-1/Assgn-1/sol_9.java
@@ -0,0 +1,10 @@
+  public class sol_9{
+    public static void main(Sting[] args){
+      
+    System.out.println(2 + "bc");         // -->  2bc
+    System.out.println(2 + 3 + "bc");     // -->  5bc
+    System.out.println((2+3) + "bc");     // -->  5bc
+    System.out.println("bc" + (2+3));     // -->  bc5
+    System.out.println("bc" + 2 + 3);     // -->  bc23
+    }
+  }
