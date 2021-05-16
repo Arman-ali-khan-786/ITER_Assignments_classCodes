9841cb391872fa3b76620bd84eb17c929d506918
ARMAN ALI KHAN <72055054+Arman-ali-khan-786@users.noreply.github.com>
2021-05-16 12:30:41 +0530

    Created sol_6.java file

---
+++ sem-1/Assgn-1/sol_6.java
@@ -0,0 +1,11 @@
+public class sol_6 {
+
+	public static void main(String[] args) {
+   int i=113;
+   double  j= 2.71828d;
+   System.out.println("This is room #"+ i);
+   System.out.println("e is close to "+ j);
+	}
+	
+
+}
