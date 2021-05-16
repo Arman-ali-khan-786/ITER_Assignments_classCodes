43250ddd4f3554ea63e6aa98340f3c1c7aacd2fa
ARMAN ALI KHAN <72055054+Arman-ali-khan-786@users.noreply.github.com>
2021-05-16 13:04:38 +0530

    Created sol_13.java file

---
+++ sem-1/Assgn-1/sol_13.java
@@ -0,0 +1,15 @@
+public class sol_13 {
+
+	public static void main(String[] args) {
+		
+  String ruler1= "1";
+  String ruler2=ruler1+" "+2+" "+ruler1;
+  String ruler3=ruler1+" "+2+" "+ruler1+" "+3+" "+ruler2;
+  String ruler4=ruler2+" "+3+" "+ruler2+" "+4+" "+ruler2+" "+3+" "+ruler2; 
+  System.out.println(ruler1);
+  System.out.println(ruler2);
+  System.out.println(ruler3);
+  System.out.println(ruler4);
+	}
+
+}
