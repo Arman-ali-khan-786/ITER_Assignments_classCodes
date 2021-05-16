0de3299fc6670a14cb35f7df8f7cbbf229bc6545
ARMAN ALI KHAN <72055054+Arman-ali-khan-786@users.noreply.github.com>
2021-05-16 13:00:51 +0530

    created sol_12.java file

---
+++ sem-1/Assgn-1/sol_12.java
@@ -0,0 +1,12 @@
+public class sol_12 {
+
+	public static void main(String[] args) {
+		
+	  double a=3.14159;
+		System.out.println(a);               //--->  3.14159
+		System.out.println(a+1);             //--->  4.14159
+		System.out.println(8/(int)a);        //--->  2
+		System.out.println(8/a);             //--->  2.5464812403910124
+		System.out.println((int)(8/a));      //--->  2
+  }
+}
