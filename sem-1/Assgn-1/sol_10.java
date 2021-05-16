552ccc2dae39df54075190815cee2bfff28543f4
ARMAN ALI KHAN <72055054+Arman-ali-khan-786@users.noreply.github.com>
2021-05-16 12:52:16 +0530

    created sol_10.java file

---
+++ sem-1/Assgn-1/sol_10.java
@@ -0,0 +1,10 @@
+public class sol_10 {
+
+	public static void main(String[] args) {
+      System.out.println('b');                 //--->  b
+	    System.out.println('b' + 'c');           //--->  197
+	    System.out.println((char) ('a' + 4));    //--->  e
+		
+	}
+
+}
