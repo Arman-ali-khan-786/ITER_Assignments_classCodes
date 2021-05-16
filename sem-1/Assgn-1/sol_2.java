5d5c657bd28972c0e28ca65a7e6e5c7cf2c23328
ARMAN ALI KHAN <72055054+Arman-ali-khan-786@users.noreply.github.com>
2021-05-16 12:00:01 +0530

    created sol_2.java file

---
+++ sem-1/Assgn-1/sol_2.java
@@ -0,0 +1,18 @@
+
+public class sol_2 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("+------------------------------+");
+		System.out.println("|                         iter |");
+		System.out.println("|                         soa  |");
+		System.out.println("|                              |");
+		System.out.println("|                              |");
+		System.out.println("|                              |");
+		System.out.println("|         ARMAN ALI KHAN       |");
+		System.out.println("|              2041013         |");
+		System.out.println("|                              |");
+		System.out.println("+------------------------------+");
+	}
+	
+}
