75680a1c25e4e1250e0cd9aba3d41c037106b28d
ARMAN ALI KHAN <72055054+Arman-ali-khan-786@users.noreply.github.com>
2021-05-16 12:27:56 +0530

    created sol_5.java file

---
+++ sem-1/Assgn-1/sol_5.java
@@ -0,0 +1,9 @@
+public class sol_5 {
+
+	public static void main(String[] args) {
+   int reg=20401013,yr=2020;
+   
+   System.out.println("My registration no is "+ reg +" and i have taken admission in B.tech in "+ yr );
+	}
+
+}
