2fa53bacf369c53790ff727eee43d0cd56340e9c
ARMAN ALI KHAN <72055054+Arman-ali-khan-786@users.noreply.github.com>
2021-05-16 12:35:24 +0530

    Created sol_7.java file

---
+++ sem-1/Assgn-1/sol_7.java
@@ -0,0 +1,16 @@
+public class sol_7 {
+
+	public static void main(String[] args) {
+    int A=5;
+    int B=6;
+    System.out.println("Before swapping");
+    System.out.println("A= " +A);
+    System.out.println("B= " +B);
+    int C =B;
+    B=A;
+    A=C;
+    System.out.println("After swapping");
+    System.out.println("A= " +A);
+    System.out.println("B= " +B);
+  }
+}
