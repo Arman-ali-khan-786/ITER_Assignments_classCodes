d070abbb32da32fecfd98a5e475257f7378a40a4
ARMAN ALI KHAN <72055054+Arman-ali-khan-786@users.noreply.github.com>
2021-05-18 20:32:01 +0530

    created sol_1.java  file

---
+++ sem-1/Assignment-2/sol_1.java
@@ -0,0 +1,10 @@
+public class assg2q1 {
+
+	public static void main(String[] args) {
+    double a=Double.parseDouble(args[0]);
+    System.out.println("enter a degree in celsius: "+a);
+    double f= (9.0/5*a)+32;
+    System.out.println("IN fahrenheit:"+f);
+	}
+
+}
