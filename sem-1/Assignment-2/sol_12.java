f9b7d3b2125c2f7fd1ae78bf8fd99b6f62ff49f4
ARMAN ALI KHAN <72055054+Arman-ali-khan-786@users.noreply.github.com>
2021-05-18 20:55:49 +0530

    change class name

--- sem-1/Assignment-2/sol_12.java
+++ sem-1/Assignment-2/sol_12.java
@@ -1,9 +1,9 @@
-public class AssgnQ12 {
+public class sol_12 {
   public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 		int N=Integer.parseInt(args[0]);
 		int gross= N/144;
-	    N= N%144;
+	         N= N%144;
 		int dz=N/12;
 		int r=N%12;
 		System.out.println("gross"+"\t"+gross+" dorzen"+"\t"+dz+" remaining egg"+"\t"+r);
