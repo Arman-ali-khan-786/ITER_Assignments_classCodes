ace8b6492bdd1c03032c45913adc596ad0802a9c
ARMAN ALI KHAN <72055054+Arman-ali-khan-786@users.noreply.github.com>
2021-05-19 22:33:10 +0530

    created sol

---
+++ sem-2/Assignment-1(dsa)/sol_9.java
@@ -0,0 +1,27 @@
+import java.util.*;
+class points{
+	int x,y;
+	void setpoint() {
+		Scanner sc= new Scanner(System.in);
+		x=sc.nextInt();
+		y=sc.nextInt();
+	}
+	double finddistance(points p1,points p2) {
+		double dis = Math.sqrt(Math.pow((p2.x-p1.x),2)+Math.pow((p2.y-p1.y), 2));
+		return dis;
+}
+}
+public class sol_9 {
+
+	public static void main(String[] args) {
+	
+    points p1=new points();
+    p1.setpoint();
+    points p2=new points();
+    p2.setpoint();
+    points p3=new points();
+    double z= p3.finddistance(p1,p2);
+    System.out.println("distance = "+z);
+	}
+
+}
