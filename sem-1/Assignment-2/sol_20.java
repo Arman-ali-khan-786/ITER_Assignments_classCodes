a81b2a59291606743d853e5096769dfe033d0e23
ARMAN ALI KHAN <72055054+Arman-ali-khan-786@users.noreply.github.com>
2021-05-23 21:09:45 +0530

    created sol_20.java file

---
+++ sem-1/Assignment-2/sol_20.java
@@ -0,0 +1,11 @@
+public class A2Q20 {
+
+	public static void main(String[] args) {
+		
+		  int m=Integer.parseInt(args[0]);
+	    int d=Integer.parseInt(args[1]);
+	    boolean s=(m==3)&&(d>=21)&&(d<=31)||(m==4)&&(d>=1)&&(d<=30)||(m==5)&&(d>=1)&&(d<=31)||(m==6)&&(d>=1)&&(d<=20);
+	    System.out.println(s);
+	}
+
+}
