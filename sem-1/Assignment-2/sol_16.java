a2aabc215c777d0364c53e9599206b9d25e82272
ARMAN ALI KHAN <72055054+Arman-ali-khan-786@users.noreply.github.com>
2021-05-23 20:51:55 +0530

    created sol_16.java file

---
+++ sem-1/Assignment-2/sol_16.java
@@ -0,0 +1,9 @@
+public class a2_q16 {
+    public static void main(String[] args) {
+        int a = 2;
+        int b = 3;
+        int p = a+(int)(Math.random()*(b-a+1));
+        int s = a+(int)(Math.random()*(b-a+1));
+        System.out.println("Sum : "+(p+s));
+   }
+}
