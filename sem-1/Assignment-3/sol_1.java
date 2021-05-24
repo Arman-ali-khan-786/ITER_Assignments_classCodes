1815c13f0fd5ac96a2d827232e491750af2cd0bb
ARMAN ALI KHAN <72055054+Arman-ali-khan-786@users.noreply.github.com>
2021-05-24 19:28:11 +0530

    created sol_1.java file

---
+++ sem-1/Assignment-3/sol_1.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Sol_1{
+  public static void main(string[] args){
+    Scanner sc = new Scanner(System.in);
+    System.out.println(" Enter height in feets : ");
+    double h =sc.nextDouble();
+    if (h>=6){
+      System.out.printn("The person is tall .");
+    }
+    sc.close();
+  }
+}
