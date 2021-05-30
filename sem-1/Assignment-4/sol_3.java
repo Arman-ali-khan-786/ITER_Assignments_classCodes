c863f51cc5dace4f5fc430d1dd24d52c76b6adf4
ARMAN ALI KHAN <72055054+Arman-ali-khan-786@users.noreply.github.com>
2021-05-30 20:07:03 +0530

    created sol_4.java file

---
+++ sem-1/Assignment-4/sol_3.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class sol_4 {
+
+	public static void main(String[] args) {
+		int i,n;
+		Scanner sc=new Scanner(System.in);
+		System.out.println("enter upto which no:");
+		n=sc.nextInt();
+		for(i=0;i<=n;i++)
+		{
+		System.out.print(i+" ");
+		}
+    sc.close();
+	}
+
+}
