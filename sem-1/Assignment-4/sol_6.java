2d8e0ac8995af2e586cadfed37556e564cce824c
ARMAN ALI KHAN <72055054+Arman-ali-khan-786@users.noreply.github.com>
2021-05-30 20:10:51 +0530

    created sol_6.java file

---
+++ sem-1/Assignment-4/sol_6.java
@@ -0,0 +1,14 @@
+public class sol_6 {
+	public static void main(String[] args) {
+		int i,ctr=0;
+		for(i=1000;i<=2000;i++)
+		{
+		System.out.print(i+" ");
+		ctr++;
+		if(ctr%5==0)
+		System.out.println();
+		}
+
+	}
+
+}
