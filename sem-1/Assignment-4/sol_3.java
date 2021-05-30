b7d06a40c022c2ae959d057010c3ade1e15dcbf8
ARMAN ALI KHAN <72055054+Arman-ali-khan-786@users.noreply.github.com>
2021-05-30 20:04:07 +0530

    created sol_3.java file

---
+++ sem-1/Assignment-4/sol_3.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class sol_3 {
+
+	public static void main(String[] args) {
+	
+		int cf,ct,cb,j;
+		Scanner sc=new Scanner(System.in);
+		System.out.print("Enter the countfrom : ");
+		cf=sc.nextInt();
+		System.out.print("Enter the countto : ");
+		ct=sc.nextInt();
+		System.out.print("Enter the countby : ");
+		cb=sc.nextInt();
+		for(j=cf;j<=ct;j+=cb)
+		{
+		System.out.print(j+" ");
+		}
+
+    sc.close();
+	}
+
+}
