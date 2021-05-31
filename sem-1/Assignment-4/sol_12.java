78d429bbf4b079c5f85a8fba9b4a3b38ebd74a4e
ARMAN ALI KHAN <72055054+Arman-ali-khan-786@users.noreply.github.com>
2021-05-31 18:54:49 +0530

    created sol_12.java file

---
+++ sem-1/Assignment-4/sol_12.java
@@ -0,0 +1,14 @@
+public class sol_12 {
+    public static void main(String[] args) {
+		int N;
+		double s=0,t=0,p=0;
+		System.out.println("log N, N, NlogN, N2, N3, 2N ");
+		for(N=16;N<=2048;N=N*2)
+		{
+		System.out.println(Math.log(N)+"\t"+N+"\t"+N*Math.log(N)+"\t"+Math.pow(N,2)+"\t"+
+		Math.pow(N, 3)+"\t"+Math.pow(2, N));
+		} 
+
+	}
+
+}
