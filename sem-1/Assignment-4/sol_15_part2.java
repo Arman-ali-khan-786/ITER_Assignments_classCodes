f5f0263b17569660fe71ebe64e4c1595102f66a4
ARMAN ALI KHAN <72055054+Arman-ali-khan-786@users.noreply.github.com>
2021-06-02 21:54:42 +0530

    created sol_15_part1.java file

---
+++ sem-1/Assignment-4/sol_15_part2.java
@@ -0,0 +1,12 @@
+public class sol_15_part2 {
+	public static void main(String[] args) {
+    //for loop
+    System.out.println("using for loop");
+	  for(int i=0;i<4;i++) {
+		  for(int j=0;j<5;j++) {
+			  System.out.print("*");
+			}
+	    System.out.println();
+  	}
+  }
+}
