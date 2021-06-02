837c491f95547fda1a2cd535c1c67fba7a6c5352
ARMAN ALI KHAN <72055054+Arman-ali-khan-786@users.noreply.github.com>
2021-06-02 21:57:07 +0530

    created sol_16_b.java file

---
+++ sem-1/Assignment-4/sol_16_b.java
@@ -0,0 +1,11 @@
+public class sol_16_b {
+	public static void main(String[] args) {
+    //part b
+     for(int i=1;i<6;i++) {
+    	 for(int j=1;j<(i+1);j++) {
+    		 System.out.print(j);
+    	 }
+    	 System.out.println();
+     }
+  }
+}
