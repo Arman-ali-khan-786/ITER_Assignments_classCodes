01e9e3a17a26935b76059510b38442f9d3f4e37e
ARMAN ALI KHAN <72055054+Arman-ali-khan-786@users.noreply.github.com>
2021-06-02 21:51:41 +0530

    created sol_15_part!.java file

---
+++ sem-1/Assignment-4/sol_15_part1.java
@@ -0,0 +1,17 @@
+public class Q15 {
+	public static void main(String[] args) {
+	
+	System.out.println("using while loop");
+//while loop
+ int i=1;
+ while(i<5) {
+	 int j=1;
+	 while(j<6) {
+		 System.out.print("*");
+		 j++;
+	 }
+	 System.out.println();
+    i++;
+ }
+	}
+}
